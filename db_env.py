f19f4c7fe9bb2a6cd5f9490de38c557afbdb1317
joshualfelipe <jlfelipe1@up.edu.ph>
2025-02-21 19:36:16 +0800

    feat: add db_env variable connections for deployment

---
+++ db_env.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+DB_CACHE_NAME = os.getenv("DB_CACHE_NAME", "")
+DB_CACHE_USER = os.getenv("DB_CACHE_USER", "")
+DB_CACHE_PASSWORD = os.getenv("DB_CACHE_PASSWORD", "")
+DB_CACHE_HOST = os.getenv("DB_CACHE_HOST", "")
+DB_CACHE_PORT = os.getenv("DB_CACHE_PORT", "")
+DB_CACHE_TABLE_NAME = os.getenv("DB_CACHE_TABLE_NAME", "")
+DB_CACHE_URL = os.getenv("DB_CACHE_URL", "")
